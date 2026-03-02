eaf4f26b9298eaf2ee328dd2f79576f22b98e13d
Pawan Tiwari <pawantiwari3943@gmail.com>
2026-03-02 20:20:46 +0530

    printing address of element

---
+++ array5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5];
+    //printing array name 
+    cout<< arr<<endl;
+    //printing address of first element 
+    cout<<&arr[0];
+    return 0;
+}
